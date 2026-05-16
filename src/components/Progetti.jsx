bf90f8d99b3ef4d8ed9c31eee57944fb5dff2fe4
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2026-05-16 21:27:44 +0200

    add clone app music

--- src/components/Progetti.jsx
+++ src/components/Progetti.jsx
@@ -10,92 +10,7 @@ function Progetti() {
             Progetti
           </h1>
           <div className="flex flex-col justify-center items-center">
-            {/*card dei progetti */}
-            <div className="card sm:card-side max-w-sm sm:max-w-full my-3 border border-amber-400 sm:h-[15em] sm:w-[100%] md:w-[90%]">
-              <figure className="h-auto  sm:w-[35%]">
-                <img
-                  src={immobili}
-                  alt="immobili"
-                  className="hover:scale-110 transition-all duration-200 ease-linear hover:cursor-pointer object-cover"
-                  onClick={() => {
-                    window.open(
-                      "https://github.com/GiuseppeTuccilli/Capstone-Gestionale-Immobili",
-                      "_blank",
-                    );
-                  }}
-                />
-              </figure>
-              <div className="card-body bg-gray-800 text-white  sm:w-[65%]">
-                <div className="overflow-y-auto">
-                  <h5 className="card-title mb-0.5 text-white">
-                    Gestionale Immobili (Front-End + Back-End)
-                  </h5>
-                  <p className="mb-2">
-                    Gestionale con registrazione e autenticazione utenti,
-                    funzionalità di recupero password.
-                  </p>
-                </div>
-
-                <div className="card-actions justify-end grow items-end">
-                  <button
-                    className="btn bg-blue-400 hover:bg-blue-200 border-amber-200 mt-auto "
-                    onClick={() => {
-                      window.open(
-                        "https://github.com/GiuseppeTuccilli/Capstone-Gestionale-Immobili",
-                        "_blank",
-                      );
-                    }}
-                  >
-                    <span className="text-2xl mb-1 icon-[tabler--brand-github]"></span>{" "}
-                    Repository
-                  </button>
-                </div>
-              </div>
-            </div>
-            {/*fine card */}
-            {/*card dei progetti */}
-            <div className="card sm:card-side max-w-sm sm:max-w-full my-3 border border-amber-400 sm:h-[15em] sm:w-[100%] md:w-[90%]">
-              <figure className="h-auto sm:w-[35%]">
-                <img
-                  src={music}
-                  alt="music"
-                  className="hover:scale-110 transition-all duration-200 ease-linear hover:cursor-pointer object-cover"
-                  onClick={() => {
-                    window.open(
-                      "https://github.com/GiuseppeTuccilli/CloneAppleMusic",
-                      "_blank",
-                    );
-                  }}
-                />
-              </figure>
-              <div className="card-body bg-gray-800 text-white sm:w-[65%]">
-                <div className="overflow-y-auto">
-                  <h5 className="card-title mb-0.5 text-white">
-                    Clone Apple Music (Front-End)
-                  </h5>
-                  <p className="mb-2">
-                    Pagina iniziale di Apple Music totalmente responsive con
-                    player e barra di ricerca funzionante.
-                  </p>
-                </div>
-                <div className="card-actions justify-end grow items-end">
-                  <button
-                    className="btn bg-blue-400 hover:bg-blue-200 border-amber-200"
-                    onClick={() => {
-                      window.open(
-                        "https://github.com/GiuseppeTuccilli/CloneAppleMusic",
-                        "_blank",
-                      );
-                    }}
-                  >
-                    <span className="text-2xl mb-1 icon-[tabler--brand-github]"></span>{" "}
-                    Repository
-                  </button>
-                </div>
-              </div>
-            </div>
-            {/*fine card */}
-            {/*card dei progetti */}
+            {/*card dei progetti meteo*/}
             <div className="card sm:card-side max-w-sm sm:max-w-full my-3 border border-amber-400 sm:h-[15em] sm:w-[100%] md:w-[90%]">
               <figure className="h-auto  sm:w-[35%]">
                 <img
@@ -104,7 +19,7 @@ function Progetti() {
                   className="hover:scale-110 transition-all duration-200 ease-linear hover:cursor-pointer object-cover"
                   onClick={() => {
                     window.open(
-                      "https://github.com/GiuseppeTuccilli/MeteoWebApp",
+                      "https://giuseppe-tuccilli-meteo-app.netlify.app",
                       "_blank",
                     );
                   }}
@@ -143,6 +58,100 @@ function Progetti() {
                     }}
                   >
                     <span className="text-2xl icon-[tabler--link]"></span> Apri
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*fine card */}
+            {/*card dei progetti  apple music*/}
+            <div className="card sm:card-side max-w-sm sm:max-w-full my-3 border border-amber-400 sm:h-[15em] sm:w-[100%] md:w-[90%]">
+              <figure className="h-auto sm:w-[35%]">
+                <img
+                  src={music}
+                  alt="music"
+                  className="hover:scale-110 transition-all duration-200 ease-linear hover:cursor-pointer object-cover"
+                  onClick={() => {
+                    window.open(
+                      "https://giuseppetuccilli-clone-appmusic.netlify.app/",
+                    );
+                  }}
+                />
+              </figure>
+              <div className="card-body bg-gray-800 text-white sm:w-[65%]">
+                <div className="overflow-y-auto">
+                  <h5 className="card-title mb-0.5 text-white">
+                    Clone Apple Music (Front-End)
+                  </h5>
+                  <p className="mb-2">
+                    Pagina iniziale di Apple Music totalmente responsive con
+                    player e barra di ricerca funzionante.
+                  </p>
+                </div>
+                <div className="card-actions justify-end grow items-end">
+                  <button
+                    className="btn bg-blue-400 hover:bg-blue-200 border-amber-200"
+                    onClick={() => {
+                      window.open(
+                        "https://github.com/GiuseppeTuccilli/CloneAppleMusic",
+                        "_blank",
+                      );
+                    }}
+                  >
+                    <span className="text-2xl mb-1 icon-[tabler--brand-github]"></span>{" "}
+                    Repository
+                  </button>
+                  <button
+                    className="btn bg-blue-400 hover:bg-blue-200 border-amber-200"
+                    onClick={() => {
+                      window.open(
+                        "https://giuseppetuccilli-clone-appmusic.netlify.app/",
+                      );
+                    }}
+                  >
+                    <span className="text-2xl icon-[tabler--link]"></span> Apri
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/*fine card */}
+            {/*card dei progetti */}
+            <div className="card sm:card-side max-w-sm sm:max-w-full my-3 border border-amber-400 sm:h-[15em] sm:w-[100%] md:w-[90%]">
+              <figure className="h-auto  sm:w-[35%]">
+                <img
+                  src={immobili}
+                  alt="immobili"
+                  className="hover:scale-110 transition-all duration-200 ease-linear hover:cursor-pointer object-cover"
+                  onClick={() => {
+                    window.open(
+                      "https://github.com/GiuseppeTuccilli/Capstone-Gestionale-Immobili",
+                      "_blank",
+                    );
+                  }}
+                />
+              </figure>
+              <div className="card-body bg-gray-800 text-white  sm:w-[65%]">
+                <div className="overflow-y-auto">
+                  <h5 className="card-title mb-0.5 text-white">
+                    Gestionale Immobili (Front-End + Back-End)
+                  </h5>
+                  <p className="mb-2">
+                    Gestionale con registrazione e autenticazione utenti,
+                    funzionalità di recupero password.
+                  </p>
+                </div>
+
+                <div className="card-actions justify-end grow items-end">
+                  <button
+                    className="btn bg-blue-400 hover:bg-blue-200 border-amber-200 mt-auto "
+                    onClick={() => {
+                      window.open(
+                        "https://github.com/GiuseppeTuccilli/Capstone-Gestionale-Immobili",
+                        "_blank",
+                      );
+                    }}
+                  >
+                    <span className="text-2xl mb-1 icon-[tabler--brand-github]"></span>{" "}
+                    Repository
                   </button>
                 </div>
               </div>
--- src/components/TopSession.jsx
+++ src/components/TopSession.jsx
@@ -13,8 +13,8 @@ function TopSession() {
         <div className=" flex justify-between px-2 py-3 border-b-2 border-blue-400 bg-linear-to-r from-blue-200 to-gray-200 flex-wrap">
           <div className="h-[5em] w-[5em] bg-cover bg-center icon-[tabler--brand-react] text-[#00D9FF] m-2 "></div>
           <div className="bg-[url('https://www.svgrepo.com/show/374093/sql.svg')] h-[5em] w-[5em] bg-cover bg-center m-2"></div>
-          <div className="bg-[url('https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/2048px-Unofficial_JavaScript_logo_2.svg.png')] h-[5em] w-[5em] bg-cover bg-center m-2"></div>
-          <div className="bg-[url('https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/2048px-HTML5_Badge.svg.png')] h-[5em] w-[5em] bg-cover bg-center m-2"></div>
+          <div className="bg-[url('public/images/Unofficial_JavaScript_logo_2.svg.png')] h-[5em] w-[5em] bg-cover bg-center m-2"></div>
+          <div className="bg-[url('public/images/HTML5_logo_and_wordmark.svg.png')] h-[5em] w-[5em] bg-cover bg-center m-2"></div>
           <div className="bg-[url('https://staging.svgrepo.com/show/134149/css.svg')] h-[5em] w-[5em] bg-cover bg-center m-2"></div>
           <div className="bg-[url('https://www.svgrepo.com/show/303388/java-4-logo.svg')] h-[5em] w-[5em] bg-cover bg-center m-2"></div>
         </div>
